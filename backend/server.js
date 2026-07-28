317d9ac9c2f244932ff9f6debb0ccab6a3697037
ruthwikachalamalla-blip <ruthwikachalamalla@gmail.com>
2026-07-28 22:26:16 +0530

    updated cors for vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = exp();
 //add cors middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://employee-phi-ruby.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
