26ba8834be910c65f47ea58ffcd745420f19b3eb
ruthwikachalamalla-blip <ruthwikachalamalla@gmail.com>
2026-07-28 20:51:10 +0530

    Updated backend changes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = exp();
 //add cors middleware
 app.use(
   cors({
-    origin: ["mongodb+srv://Ruthwika:ruthwika123@cluster0.fmrvzwm.mongodb.net/?appName=Cluster0"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
